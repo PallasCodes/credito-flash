7d622ba7b1d639701564cbea702c0d1db8b9464d
pallascodes <pallascodes@gmail.com>
2024-11-13 17:07:02 -0600

    feat: PersonaFisica entity agregada

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column, PrimaryColumn } from 'typeorm'
+import { PersonaFisica } from 'src/solicitud/entities/PersonaFisica.entity'
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn } from 'typeorm'
 
 @Entity({ name: 'usuarioCreditoFlash', schema: 'web' })
 export class User {
@@ -8,8 +9,9 @@ export class User {
   @Column({ type: 'varchar', length: 13, nullable: true, name: 'rfc' })
   rfc: string
 
-  @Column({ type: 'int', nullable: true, name: 'idPersonaFisica' })
-  idPersonaFisica: number
+  @OneToOne(() => PersonaFisica)
+  @JoinColumn({ name: 'idPersonaFisica' })
+  personaFisica: PersonaFisica
 
   @Column({ type: 'varchar', length: 250, nullable: true, name: 'contrasena' })
   contrasena: string
--- src/solicitud/solicitud.module.ts
+++ src/solicitud/solicitud.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common'
 import { SolicitudService } from './solicitud.service'
 import { SolicitudController } from './solicitud.controller'
 import { AuthModule } from 'src/auth/auth.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { PersonaFisica } from './entities/PersonaFisica.entity'
 @Module({
   controllers: [SolicitudController],
   providers: [SolicitudService],
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forFeature([PersonaFisica])],
 })
 export class SolicitudModule {}
