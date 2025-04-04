d0c713c66864f6ed600e1ac9513c07379fab1fdc
pallascodes <pallascodes@gmail.com>
2025-04-04 12:08:33 -0600

    feat: integrate TypeORM for EventoOpenpay entity in Openpay module

--- src/openpay/openpay.module.ts
+++ src/openpay/openpay.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { OpenpayService } from './openpay.service';
-import { OpenpayController } from './openpay.controller';
+import { Module } from '@nestjs/common'
+import { OpenpayService } from './openpay.service'
+import { OpenpayController } from './openpay.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { EventoOpenpay } from './entities/evento-openpay.entity'
 
 @Module({
   controllers: [OpenpayController],
-  providers: [OpenpayService]
+  providers: [OpenpayService],
+  imports: [TypeOrmModule.forFeature([EventoOpenpay])],
 })
 export class OpenpayModule {}
